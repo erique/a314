4faf9a81e218f730b7ce03d61474571990668bd5
Erik Hemming <eriquesnk@gmail.com>
2024-11-18 23:17:21 +0100

    cleanup Makefile

--- local.sh
+++ local.sh
@@ -5,5 +5,7 @@ NDK32=/opt/ndk32
 
 PATH=$PATH:$VBCC/bin
 
-make -j -C Software
+BIN=bin_pi
+make -j -C Software ${BIN}/a314d-tf4060 ${BIN}/spi-a314.dtbo
+
 VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j
