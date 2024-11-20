dc6c2fff0f7440d30d3dd1aaeb656e402bbd63ba
Erik Hemming <eriquesnk@gmail.com>
2024-11-21 00:27:50 +0100

    better handling of failed deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 set -xe
 sudo cp Software/bin_pi/a314d-tf4060 /opt/a314/a314d
 cd Software/bin_amiga/Devs
-~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device || true
-~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314eth.device devs: || true
+~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device || echo "failed to deploy"
+~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314eth.device devs: || echo "failed to deploy"
 cd -
