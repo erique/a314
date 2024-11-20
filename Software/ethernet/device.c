c12e0d8ca33ebe613ad31350116c3664c2c7adb9
Erik Hemming <eriquesnk@gmail.com>
2024-11-21 00:28:47 +0100

    debug output from a314eth

--- Software/ethernet/device.c
+++ Software/ethernet/device.c
@@ -23,6 +23,9 @@
 
 #include "../a314device/a314.h"
 #include "../a314device/proto_a314.h"
+#include "../a314device/kprintf.h"
+
+#define TRACE(func) do{ kprintf("A314eth: [%s:%ld]\n", func, __LINE__); }while(0)
 
 // Defines.
 
@@ -146,6 +149,8 @@ extern void device_process_seglist();
 
 static struct Library *init_device(__reg("a6") struct ExecBase *sys_base, __reg("a0") BPTR seg_list, __reg("d0") struct Library *dev)
 {
+;;	TRACE("init_device");
+
 	saved_seg_list = seg_list;
 
 	dev->lib_Node.ln_Type = NT_DEVICE;
@@ -163,6 +168,8 @@ static struct Library *init_device(__reg("a6") struct ExecBase *sys_base, __reg(
 
 static BPTR expunge(__reg("a6") struct Library *dev)
 {
+;;	TRACE("expunge");
+
 	if (dev->lib_OpenCnt)
 	{
 		dev->lib_Flags |= LIBF_DELEXP;
@@ -181,6 +188,8 @@ static BPTR expunge(__reg("a6") struct Library *dev)
 
 static void send_a314_cmd(struct A314_IORequest *ior, UWORD cmd, char *buffer, int length)
 {
+;;	TRACE("send_a314_cmd");
+
 	ior->a314_Request.io_Command = cmd;
 	ior->a314_Request.io_Error = 0;
 	ior->a314_Socket = a314_socket;
@@ -191,6 +200,8 @@ static void send_a314_cmd(struct A314_IORequest *ior, UWORD cmd, char *buffer, i
 
 static void do_a314_cmd(struct A314_IORequest *ior, UWORD cmd, char *buffer, int length)
 {
+;;	TRACE("do_a314_cmd");
+
 	ior->a314_Request.io_Command = cmd;
 	ior->a314_Request.io_Error = 0;
 	ior->a314_Socket = a314_socket;
@@ -201,6 +212,8 @@ static void do_a314_cmd(struct A314_IORequest *ior, UWORD cmd, char *buffer, int
 
 static void copy_from_bd_and_reply(struct IOSana2Req *ios2, struct BufDesc *bd)
 {
+;;	TRACE("copy_from_bd_and_reply");
+
 	// TODO: Use S2_DMACopyToBuff32 instead of S2_CopyToBuff (if possible).
 	// This will avoid copying from A314 shared memory to shadow buffer
 	// before copying to stack's buffer.
@@ -248,6 +261,9 @@ static void copy_from_bd_and_reply(struct IOSana2Req *ios2, struct BufDesc *bd)
 
 static void copy_to_bd_and_reply(struct BufDesc *bd, struct IOSana2Req *ios2)
 {
+;;	TRACE("copy_to_bd_and_reply");
+
+		kprintf("A314eth: ios2 = %08lx\n", ios2);
 	// TODO: Use S2_DMACopyFromBuff32 instead of S2_CopyFromBuff (if possible).
 	// This will avoid copying to shadow buffer before writing to
 	// A314 shared memory.
@@ -256,11 +272,18 @@ static void copy_to_bd_and_reply(struct BufDesc *bd, struct IOSana2Req *ios2)
 
 	if (ios2->ios2_Req.io_Flags & SANA2IOF_RAW)
 	{
+		kprintf("A314eth: RAW\n");
+
 		copyfrom(bd->bd_Buffer, ios2->ios2_Data, ios2->ios2_DataLength);
 		bd->bd_Length = ios2->ios2_DataLength;
 	}
 	else
 	{
+		kprintf("A314eth: COOKED\n");
+
+		kprintf("A314eth: eh = %08lx\n", eh);
+		kprintf("A314eth: ios2->ios2_Data = %08lx\n", ios2->ios2_Data);
+
 		eh->eh_Type = ios2->ios2_PacketType;
 		memcpy(eh->eh_Src, macaddr, sizeof(macaddr));
 		memcpy(eh->eh_Dst, ios2->ios2_DstAddr, MACADDR_SIZE);
@@ -268,7 +291,11 @@ static void copy_to_bd_and_reply(struct BufDesc *bd, struct IOSana2Req *ios2)
 		bd->bd_Length = ios2->ios2_DataLength + sizeof(struct EthHdr);
 	}
 
+	kprintf("About to copy\n");
+
 	WriteMemA314(bd->bd_BufferAddress, bd->bd_Buffer, bd->bd_Length);
+
+	kprintf("About to reply\n");
 
 	ios2->ios2_Req.io_Error = 0;
 	ReplyMsg(&ios2->ios2_Req.io_Message);
@@ -278,6 +305,8 @@ static void copy_to_bd_and_reply(struct BufDesc *bd, struct IOSana2Req *ios2)
 
 static void handle_a314_reply(struct A314_IORequest *ior)
 {
+;;	TRACE("handle_a314_reply");
+
 	if (ior == &write_ior)
 	{
 		pending_a314_write = FALSE;
@@ -325,6 +354,8 @@ static void handle_a314_reply(struct A314_IORequest *ior)
 
 static struct IOSana2Req *remove_matching_rbuf(ULONG type)
 {
+;;	TRACE("remove_matching_rbuf");
+
 	struct Node *node = ut_rbuf_list.lh_Head;
 	while (node->ln_Succ)
 	{
@@ -341,6 +372,8 @@ static struct IOSana2Req *remove_matching_rbuf(ULONG type)
 
 static void complete_read_reqs()
 {
+;;	TRACE("complete_read_reqs");
+
 	struct Node *node = et_rbuf_has_data_list.lh_Head;
 	if (!node->ln_Succ)
 		return;
@@ -370,6 +403,8 @@ static void complete_read_reqs()
 
 static void maybe_write_req()
 {
+;;	TRACE("maybe_write_req");
+
 	if (pending_a314_write)
 		return;
 
@@ -429,6 +464,8 @@ static void maybe_write_req()
 
 void device_process_run()
 {
+;;	TRACE("device_process_run");
+
 	ULONG sana2_signal = AllocSignal(-1);
 	sana2_sigmask = 1UL << sana2_signal;
 
@@ -483,6 +520,8 @@ void device_process_run()
 
 static struct TagItem *FindTagItem(Tag tagVal, struct TagItem *tagList)
 {
+;;	TRACE("FindTagItem");
+
 	struct TagItem *ti = tagList;
 	while (ti && ti->ti_Tag != tagVal)
 	{
@@ -507,12 +546,15 @@ static struct TagItem *FindTagItem(Tag tagVal, struct TagItem *tagList)
 
 static ULONG GetTagData(Tag tagVal, ULONG defaultData, struct TagItem *tagList)
 {
+;;	TRACE("GetTagData");
 	struct TagItem *ti = FindTagItem(tagVal, tagList);
 	return ti ? ti->ti_Data : defaultData;
 }
 
-static void open(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req *ios2, __reg("d0") ULONG unitnum, __reg("d1") ULONG flags)
+void open(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req *ios2, __reg("d0") ULONG unitnum, __reg("d1") ULONG flags)
 {
+;;	TRACE("open");
+
 	ios2->ios2_Req.io_Error = IOERR_OPENFAIL;
 	ios2->ios2_Req.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
 
@@ -524,6 +566,9 @@ static void open(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req 
 	copyfrom = (buf_copy_func_t)GetTagData(S2_CopyFromBuff, 0, (struct TagItem *)ios2->ios2_BufferManagement);
 	copyto = (buf_copy_func_t)GetTagData(S2_CopyToBuff, 0, (struct TagItem *)ios2->ios2_BufferManagement);
 	ios2->ios2_BufferManagement = (void *)0xdeadbeefUL;
+
+	kprintf("A314eth: copyfrom = %08lx\n", copyfrom);
+	kprintf("A314eth: copyto   = %08lx\n", copyto);
 
 	memset(&a314_mp, 0, sizeof(a314_mp));
 	a314_mp.mp_Node.ln_Pri = 0;
@@ -573,10 +618,12 @@ static void open(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req 
 		struct BufDesc *bd = &et_bufs[i];
 
 		bd->bd_Buffer = AllocMem(RAW_MTU, 0);
+		kprintf("A314eth: bd_Buffer = %08lx\n", bd->bd_Buffer);
 		if (!bd->bd_Buffer)
 			goto error;
 
 		bd->bd_BufferAddress = AllocMemA314(RAW_MTU);
+		kprintf("A314eth: bd_BufferAddress = %08lx\n", bd->bd_BufferAddress);
 		if (bd->bd_BufferAddress == INVALID_A314_ADDRESS)
 			goto error;
 
@@ -623,6 +670,8 @@ error:
 
 static BPTR close(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req *ios2)
 {
+;;	TRACE("close");
+
 	init_task = FindTask(NULL);
 	Signal(&device_process->pr_Task, shutdown_sigmask);
 	Wait(SIGF_SINGLE);
@@ -649,6 +698,8 @@ static BPTR close(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req
 
 static void device_query(struct IOSana2Req *req)
 {
+;;	TRACE("device_query");
+
 	struct Sana2DeviceQuery *query;
 
 	query = req->ios2_StatData;
@@ -672,6 +723,8 @@ static void device_query(struct IOSana2Req *req)
 
 static void set_last_start()
 {
+;;	TRACE("set_last_start");
+
 	struct IORequest req;
 	memset(&req, 0, sizeof(req));
 	req.io_Message.mn_Length = sizeof(req);
@@ -686,12 +739,15 @@ static void set_last_start()
 
 static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req *ios2)
 {
+;;	TRACE("begin_io");
+
 	ios2->ios2_Req.io_Error = S2ERR_NO_ERROR;
 	ios2->ios2_WireError = S2WERR_GENERIC_ERROR;
 
 	switch (ios2->ios2_Req.io_Command)
 	{
 	case CMD_READ:
+;;	TRACE("CMD_READ");
 		if (!ios2->ios2_BufferManagement)
 		{
 			ios2->ios2_Req.io_Error = S2ERR_BAD_ARGUMENT;
@@ -710,10 +766,12 @@ static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2
 		break;
 
 	case S2_BROADCAST:
+;;	TRACE("S2_BROADCAST");
 		memset(ios2->ios2_DstAddr, 0xff, MACADDR_SIZE);
 		/* Fall through */
 
 	case CMD_WRITE:
+;;	TRACE("CMD_WRITE");
 		if (((ios2->ios2_Req.io_Flags & SANA2IOF_RAW) != 0 && ios2->ios2_DataLength > RAW_MTU) ||
 			((ios2->ios2_Req.io_Flags & SANA2IOF_RAW) == 0 && ios2->ios2_DataLength > ETH_MTU))
 		{
@@ -739,23 +797,28 @@ static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2
 		break;
 
 	case S2_CONFIGINTERFACE:
+;;	TRACE("S2_CONFIGIFACE");
 		/* Fall through */
 
 	case S2_ONLINE:
+;;	TRACE("S2_ONLINE");
 		set_last_start();
 		is_online = TRUE;
 		break;
 
 	case S2_OFFLINE:
+;;	TRACE("S2_OFFLINE");
 		is_online = FALSE;
 		break;
 
 	case S2_GETSTATIONADDRESS:
+;;	TRACE("S2_GETSTATIONADDRESS");
 		memcpy(ios2->ios2_SrcAddr, macaddr, sizeof(macaddr));
 		memcpy(ios2->ios2_DstAddr, macaddr, sizeof(macaddr));
 		break;
 
 	case S2_DEVICEQUERY:
+;;	TRACE("S2_DEVICEQUERY");
 		device_query(ios2);
 		break;
 
@@ -765,14 +828,17 @@ static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2
 	case S2_GETTYPESTATS:
 	case S2_READORPHAN:
 	case S2_GETSPECIALSTATS:
+;;	TRACE("S2_tracking...");
 		break;
 
 	case S2_GETGLOBALSTATS:
+;;	TRACE("S2_GETBLOBALSTATS");
 		if (ios2->ios2_StatData)
 			memcpy(ios2->ios2_StatData, &global_stats, sizeof(struct Sana2DeviceStats));
 		break;
 
 	default:
+;;	TRACE("DEFAULT NOCMD");
 		ios2->ios2_Req.io_Error = IOERR_NOCMD;
 		ios2->ios2_WireError = S2WERR_GENERIC_ERROR;
 		break;
@@ -780,15 +846,26 @@ static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2
 
 	if (ios2)
 	{
+;;	TRACE("  ios2");
 		if (ios2->ios2_Req.io_Flags & SANA2IOF_QUICK)
+		{
+;;	TRACE("     quick");
 			ios2->ios2_Req.io_Message.mn_Node.ln_Type = NT_MESSAGE;
+		}
 		else
+		{
+;;	TRACE("     reply");
 			ReplyMsg(&ios2->ios2_Req.io_Message);
+		}
+
 	}
+;;	TRACE("< done");
 }
 
 static void remove_from_list(struct List *list, struct Node *node)
 {
+;;	TRACE("remove_from_list");
+
 	for (struct Node *n = list->lh_Head; n->ln_Succ; n = n->ln_Succ)
 	{
 		if (n == node)
@@ -801,6 +878,8 @@ static void remove_from_list(struct List *list, struct Node *node)
 
 static ULONG abort_io(__reg("a6") struct Library *dev, __reg("a1") struct IOSana2Req *ios2)
 {
+;;	TRACE("abort_io");
+
 	Forbid();
 	remove_from_list((struct List *)&ut_rbuf_list, &ios2->ios2_Req.io_Message.mn_Node);
 	remove_from_list((struct List *)&ut_wbuf_list, &ios2->ios2_Req.io_Message.mn_Node);
