35daa29605537ac2c720741ed99895ed07b0ef6b
Erik Hemming <eriquesnk@gmail.com>
2024-11-20 23:17:53 +0100

    add a314eth to scripts

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 
 set -xe
 sudo cp Software/bin_pi/a314d-tf4060 /opt/a314/a314d
-cd Software/bin_amiga/Devs && ~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device
+cd Software/bin_amiga/Devs
+~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device || true
+~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314eth.device devs: || true
+cd -
--- local.sh
+++ local.sh
@@ -9,4 +9,4 @@ BIN=bin_pi
 make -j -C Software ${BIN}/a314d-tf4060 ${BIN}/spi-a314.dtbo
 
 BIN=bin_amiga
-VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j ${BIN}/Devs/a314-tf4060.device
+VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j ${BIN}/Devs/a314-tf4060.device ${BIN}/Devs/a314eth.device
