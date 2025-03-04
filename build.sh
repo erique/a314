61b69eeb2b6310b8b620b74bc81e925dc99797d2
Erik Hemming <eriquesnk@gmail.com>
2025-03-05 00:12:38 +0100

    Reset local.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ NDK32=/opt/ndk32
 
 PATH=$PATH:$VBCC/bin
 
-make -j -C Software
+BIN=bin_pi
+make -j -C Software ${BIN}/a314d-tf4060 ${BIN}/spi-a314.dtbo
 
-VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j
+BIN=bin_amiga
+VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j ${BIN}/Devs/a314-tf4060.device
