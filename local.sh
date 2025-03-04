6f3e4d6b3091bca79bf76894b606d005f56b3e64
Erik Hemming <eriquesnk@gmail.com>
2025-03-05 00:12:38 +0100

    Reset local.sh

--- local.sh
+++ local.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-VBCC=/opt/vbcc
-NDK32=/opt/ndk32
-
-PATH=$PATH:$VBCC/bin
-
-BIN=bin_pi
-make -j -C Software ${BIN}/a314d-tf4060 ${BIN}/spi-a314.dtbo
-
-BIN=bin_amiga
-VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j ${BIN}/Devs/a314-tf4060.device ${BIN}/Devs/a314eth.device
+make -C Software -j
+VBCC=/opt/vbcc PATH=$PATH:$VBCC/bin NDK32=/opt/ndk32 make -C Software -f Makefile-amiga -j
