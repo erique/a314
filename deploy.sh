89ce0ccc019feaf1e1f29198aa191fa2c54a2ee1
Erik Hemming <eriquesnk@gmail.com>
2025-03-05 15:11:47 +0100

    deploy via a314eth

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -xe
-sudo cp Software/bin_pi/a314d-tf4060 /opt/a314/a314d
+sudo cp Software/bin_pi/a314d-tf4060 /opt/a314/a314d || echo "failed to deploy"
 cd Software/bin_amiga/Devs
-~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device || echo "failed to deploy"
+#~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.10.23 c:copy a314-tf4060.device devs:a314.device || echo "failed to deploy"
+~/rlaunch/t2-output/linux-gcc-release-default/rl-controller 192.168.2.2 c:copy a314-tf4060.device devs:a314.device || echo "failed to deploy"
 cd -
