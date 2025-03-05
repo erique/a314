93b358a4e06ee053f942be78712daa2b12b665e0
Erik Hemming <eriquesnk@gmail.com>
2025-03-05 01:14:34 +0100

    -j

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -C Software
-VBCC=/opt/vbcc PATH=$PATH:$VBCC/bin NDK32=/opt/ndk32 make -C Software -f Makefile-amiga
+make -C Software -j
+VBCC=/opt/vbcc PATH=$PATH:$VBCC/bin NDK32=/opt/ndk32 make -C Software -f Makefile-amiga -j
