b805a0cbdf72be1f248cc4e9f4847ecd15b2a44e
Erik Hemming <eriquesnk@gmail.com>
2025-03-07 08:37:41 +0100

    fix install script and rename to a314d-tf/a314-tf.device

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ NDK32=/opt/ndk32
 PATH=$PATH:$VBCC/bin
 
 BIN=bin_pi
-make -j -C Software ${BIN}/a314d-tf4060 ${BIN}/spi-a314.dtbo
+make -j -C Software ${BIN}/a314d-tf ${BIN}/spi-a314-tf.dtbo
 
 BIN=bin_amiga
-VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j ${BIN}/Devs/a314-tf4060.device
+VBCC=$VBCC NDK32=$NDK32 make -j -C Software -f Makefile-amiga -j bin_dir ${BIN}/Devs/a314-tf.device
